2c2e4ff683b765923827657c341aac508ae7319e
jordi <jserra91@gmail.com>
2021-03-04 11:22:42 +0000

    finish forms

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,57 @@ import './App.css';
 import InputText from './Components/InputText/InputText';
 
 function App() {
-
   const [name, setName] = React.useState('');
+  const [surname, setSurname] = React.useState('');
+  const [list, setList] = React.useState([]);
 
-  const handleName = (event: any) => {
-    setName(event.target.value);
+  const handleName = (value: any) => {
+    setName(value);
+  }
+
+  const handleSurname = (value: any) => {
+    setSurname(value);
+  }
+
+  const onSubmit = (event: any) => {
+    event.preventDefault();
+    const auxList = [
+      ...list,
+      {
+        name: name,
+        surname: surname
+      }
+    ];
+    setList(auxList as []);
+    setName('');
+    setSurname('');
   }
 
   return (
     <>
       <div className="App">
-        <InputText value={name} handlechange={handleName} />
+        <form onSubmit={onSubmit}>
+          <div>
+            <InputText label="Name" value={name} handlechange={handleName} />
+          </div>
+          <div>
+            <InputText label="Surname" value={surname} handlechange={handleSurname} />
+          </div>
+          <div>
+            <button type="submit">Send data</button>
+          </div>
+        </form>
         <div>
-          {name}
+          {
+            list.map((value: any, index: number) => {
+              return (
+                <div>
+                  <div>Name: {value.name}</div>
+                  <div>Surname: {value.surname}</div>
+                </div>
+              );
+            })
+          }
         </div>
       </div>
     </>
--- src/Components/InputText/InputText.tsx
+++ src/Components/InputText/InputText.tsx
@@ -2,23 +2,31 @@ import * as React from 'react';
 
 export interface IInputTextProps {
     value?: string;
+    label?: string;
+    color?: "yellow" | "red" | "green";
     handlechange(value: any): void;
 }
 
 export const InputText: React.FC<IInputTextProps> = (props: IInputTextProps) => {
 
     const [value, setValue] = React.useState(props.value ? props.value : '');
+    const [label, setLabel] = React.useState(props.label ? props.label : '');
 
     React.useEffect(
         () => {
             setValue(props.value ? props.value : '');
         },
-        [props.value])
+        [props.value]);
+
+    const handleChange = (event: any) => {
+        props.handlechange(event.target.value);
+    }
 
     return (
         <>
             <div>
-                <input type="text" value={value} onChange={props.handlechange} />
+                {label}
+                <input type="text" value={value} onChange={handleChange} />
             </div>
             {value && value.length < 5 && <div>
                 Minium required 5!!!
